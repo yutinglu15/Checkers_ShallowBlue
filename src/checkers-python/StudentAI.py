ba37a819e7b3e4793d17868f97a0080e4b55f830
yutin <493806061@qq.com>
2019-11-18 19:14:26 -0800

    update stupid ml not yet implemented

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -3,158 +3,7 @@ from BoardClasses import Move
 from BoardClasses import Board
 import math
 import copy
-import time
-
-class Node():
-    def __init__(self, board, parent, turn):
-        self.board = copy.deepcopy(board)
-        self.parent = parent
-        self.win = 0
-        self.visit = 0
-        self.turn = turn
-
-
-    def ucb(self, c):
-        return self.win/self.visit + c*math.sqrt(math.log(self.parent.visit)/self.visit)
-
-
-class MonteCarloTree():
-    def __init__(self, board, moves, color, opponent):
-        self.board = board
-        self.moves = moves
-        self.color = color
-        self.opponent = opponent
-        self.root = Node(board, None, self.color)
-        self.states = {str(self.board.board): self.root} # {board: node}
-
-    def get_action(self, simulate_time):
-        # curr_move = self.select(self.root, self.color)
-
-        start_time = time.time()
-        while time.time() - start_time < simulate_time:
-            select_node = self.select(self.root, self.root.turn)
-            w = self.simulate(select_node.board, select_node.turn)
-            self.backpropagate(select_node, w)
-
-        # select the best child
-        best_move = self.select(self.root, self.color).move
-
-        return best_move
-
-
-
-    # random roll a move
-    def rollout(self, moves):
-        index = randint(0,len(moves)-1)
-        inner_index = randint(0,len(moves[index])-1)
-        move = moves[index][inner_index]
-        return move
-
-
-
-    def best_child(self, children):
-        best_ucb = 0
-        best_node = None
-        for child in children:
-            ucb = child.ucb(1.4)
-            if ucb > best_ucb:
-                best_ucb = ucb
-                best_node = child
-        return best_node
-
-
-    def select(self, node, turn):
-        board = node.board
-        moves = board.get_all_possible_moves(turn)
-
-        # explore all possible children
-        children = []
-        for chess in moves:
-            for move in chess:
-                board.make_move(move, turn)
-
-                # if we have seen this board
-                if str(board.board)+str(turn) in self.states:
-                    children.append(self.states[str(board.board)+str(turn)])
-                    board.undo()
-                    continue
-
-                # else we expand and simulate
-                child = Node(board, node, turn)
-                child.move = move  # get the board with which move
-                children.append(child)
-                self.states[str(board.board)+str(turn)] = child
-
-                win = self.simulate(board, turn)
-                child.win = win
-                child.visit = 1
-                self.backpropagate(child, win)
-
-                board.undo()
-
-        return self.best_child(children)
-
-
-    # # not sure about how to implement it yet...
-    # def expand(self):
-    #     board = self.board
-    #     t = 0
-    #     curr_node = self.get_node(board)
-    #     while curr_node:
-    #         moves = board.get_all_possible_moves(self.color)
-    #         move = self.rollout(moves)
-    #         board.make_move(move, self.color)
-    #         t += 1
-    #
-    #         moves = board.get_all_possible_moves(self.opponent[self.color])
-    #         move = self.rollout(moves)
-    #         board.make_move(move, self.opponent[self.color])
-    #         t += 1
-    #
-    #         curr_node = self.get_node(board)
-    #
-    #     # now curr_node is unexplored
-    #     curr_node = Node(board)
-    #
-    #     self.undo(board, t)
-
-
-    # simulate the game with a start move
-    def simulate(self, board, color):
-        win = 0
-        curr_turn = self.opponent[color]
-        t = 0
-        for turn in range(20):
-            if board.is_win(color) == color:
-                win = 1
-                self.undo(board,t)
-                break
-            elif board.is_win(self.opponent[color]) == self.opponent[color]:
-                self.undo(board, t)
-                break
-            move = self.rollout(board.get_all_possible_moves(curr_turn))
-            board.make_move(move, curr_turn)
-            curr_turn = self.opponent[curr_turn]
-            t += 1
-        else:
-            win += 0.5
-            self.undo(board, t)
-        return win
-
-
-    def backpropagate(self, node, win):
-        if not node.parent:
-            return
-        node.parent.win += win
-        node.parent.visit += 1
-        self.backpropagate(node.parent, win)
-
-
-    def undo(self, board, times):
-        for _ in range(times):
-            board.undo()
-
-
+import random
 
 #The following part should be completed by students.
 #Students can modify anything except the class name and exisiting functions and varibles.
@@ -169,16 +18,198 @@ class StudentAI():
         self.color = ''
         self.opponent = {1:2,2:1}
         self.color = 2
-    def get_move(self,move):
+        self.movecount = 0
+
+    def get_move(self, move):
+        #print(self.color)
         if len(move) != 0:
             self.board.make_move(move,self.opponent[self.color])
         else:
             self.color = 1
         moves = self.board.get_all_possible_moves(self.color)
-        # index = randint(0,len(moves)-1)
-        # inner_index =  randint(0,len(moves[index])-1)
-        # move = moves[index][inner_index]
-        mct = MonteCarloTree(self.board, moves, self.color, self.opponent)
-        move = mct.get_action(10)
+        self.train()
+
+        #index = randint(0,len(moves)-1)
+        #inner_index =  randint(0,len(moves[index])-1)
+        #move = moves[index][inner_index]
+        #print(moves)
+        move = self.minimax_move(moves)
+        #move = self.monte_carlo_tree([m for chess in moves for m in chess], 10, 10)
         self.board.make_move(move, self.color)
+        self.movecount += 1
         return move
+
+    def minimax_move(self, moves: [list]) :
+        best = []
+        max_value = - math.inf
+        depth = 4
+        for chess in moves:
+            for move in chess:
+                val = self.max_value(move, depth, -math.inf, math.inf)
+                if val > max_value:
+                    best = [move]
+                    max_value = val
+                elif val == max_value:
+                    best.append(move)
+        if len(best) == 1:
+            return best[randint(0,len(best)-1)]
+        #print(len(best))
+        return self.monte_carlo_tree(best, 10, 10)
+
+    def monte_carlo_tree(self, moves: [], simulate_times: int, s_parent: int):
+        best_uct = 0
+        best_move = 0
+        for move in moves:
+            wins = self.simulate(move, simulate_times)
+            uct = wins/simulate_times + math.sqrt(2*math.log(s_parent)/simulate_times)
+            if uct > best_uct:
+                best_move = move
+        return best_move
+
+    def simulate(self, move, s):
+        wins = 0
+        board = copy.deepcopy(self.board)
+        board.make_move(move, self.color)
+        for i in range(s):
+            curr_turn = self.opponent[self.color]
+            t = 0
+            for turn in range(20):
+                if board.is_win(self.color) == self.color:
+                    wins = 1
+                    self.undo(board,t)
+                    break
+                elif board.is_win(self.opponent[self.color]) == self.opponent[self.color]:
+                    self.undo(board, t)
+                    break
+                moves = board.get_all_possible_moves(curr_turn)
+                index = randint(0,len(moves)-1)
+                inner_index =  randint(0,len(moves[index])-1)
+                board.make_move(moves[index][inner_index], curr_turn)
+                curr_turn = self.opponent[curr_turn]
+                t += 1
+            else:
+                wins = 0.5
+                self.undo(board, t)
+
+        return wins
+
+    def undo(self, board, times):
+        for _ in range(times):
+            board.undo()
+
+    def min_value(self, move, depth, alpha, beta):
+        self.board.make_move(move, self.opponent[self.color])
+        if depth == 0:
+            u = self.utility(self.board)
+            self.board.undo()
+            return u
+        min_val = math.inf
+        for chess in self.board.get_all_possible_moves(self.color):
+            for move in chess:
+                min_val = min(self.max_value(move, depth - 1, alpha, beta), min_val)
+                beta = min(beta, min_val)
+                if alpha >= beta:
+                    self.board.undo()
+                    return min_val
+        self.board.undo()
+        return min_val
+
+    def max_value(self, move, depth, alpha, beta):
+        self.board.make_move(move, self.color)
+        if depth == 0:
+            u = self.utility(self.board)
+            self.board.undo()
+            return u
+        max_val = - math.inf
+        for chess in self.board.get_all_possible_moves(self.opponent[self.color]):
+            for move in chess:
+                max_val = max(self.min_value(move, depth - 1, alpha, beta), max_val)
+                alpha = max(alpha, max_val)
+                if alpha >= beta:
+                    self.board.undo()
+                    return max_val
+        self.board.undo()
+        return max_val
+
+    def utility(self, board):
+        bking, wking = 0, 0
+        for r in range(self.board.row):
+            for c in range(self.board.col):
+                if self.board.board[r][c].color == "B":
+                    bking += self.board.board[r][c].is_king
+                elif self.board.board[r][c].color == "W":
+                    wking += self.board.board[r][c].is_king
+
+        bback = sum(self.board.board[0][i].color == "B" for i in range(self.board.col))
+        wback = sum(self.board.board[self.board.row-1][i].color == "W" for i in range(self.board.col))
+
+        bedge = sum(self.board.board[i][0].color == "B"+self.board.board[i][self.board.col-1].color == "B" for i in range(self.board.row))
+        wedge = sum(self.board.board[i][0].color == "W"+self.board.board[i][self.board.col-1].color == "W" for i in range(self.board.row))
+
+        time_param = math.log(self.movecount)
+        b = 3*self.board.black_count + bking * time_param + bback * (1/time_param) + bedge * (1/time_param)
+        w = 3*self.board.white_count + wking * time_param + wback * (1/time_param) + wedge * (1/time_param)
+        # b = self.board.black_count
+        # w = self.board.white_count
+        #print("black:",b," white:",w)
+        return b-w if self.color == 1 else w-b
+
+
+    ####################################################
+    ### Training heuristics using Linear Regression ####
+    ####################################################
+
+    def get_X(self, board):
+        bking, wking = 0, 0
+        for r in range(self.board.row):
+            for c in range(self.board.col):
+                if self.board.board[r][c].color == "B":
+                    bking += self.board.board[r][c].is_king
+                elif self.board.board[r][c].color == "W":
+                    wking += self.board.board[r][c].is_king
+
+        bback = sum(self.board.board[0][i].color == "B" for i in range(self.board.col))
+        wback = sum(self.board.board[self.board.row-1][i].color == "W" for i in range(self.board.col))
+
+        bedge = sum(self.board.board[i][0].color == "B"+self.board.board[i][self.board.col-1].color == "B" for i in range(self.board.row))
+        wedge = sum(self.board.board[i][0].color == "W"+self.board.board[i][self.board.col-1].color == "W" for i in range(self.board.row))
+
+        X_black = [self.board.black_count, bking, bback, bedge, self.movecount]
+        X_white = [self.board.white_count, wking, wback, wedge, self.movecount]
+
+        return (X_black, X_white)
+
+    def model(self, X, thetas):
+        # X = [count, king, back, edge, time]
+        # thetas = [t1, t2, t3, t4, t5]
+        return thetas[0] * X[0] + thetas[1] * X[1] + thetas[2] * X[2] + thetas[3] * X[3] + thetas[4] * X[4]
+
+
+    def train(self):
+        thetas = [random.random() for i in range(5)]
+
+        epoch = 0.01
+        alpha = 0.05
+
+        while True:
+            thetas = thetas - self.Gradient() * alpha
+            if self.Gradient() * alpha < epoch:
+                break
+
+        return thetas
+
+
+
+
+    def MSE(self, thetas):
+        # get mean square error by simulation
+        mse = 0
+        new_board = copy.deepcopy(self.board)
+
+        utility = self.model(self.get_X(self.board), thetas)
+
+        return mse
+
+    def Gradient(self, mse):
+        # calculate gradient according to mse
+        pass

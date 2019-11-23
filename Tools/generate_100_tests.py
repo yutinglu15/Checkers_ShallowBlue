1d56957c36e7a9270e298fed50fd048315a8d40a
Tinameow <dingtingyin@foxmail.com>
2019-11-23 00:53:55 -0800

    Add the heuristic: difference in total distance from home

---
+++ Tools/generate_100_tests.py
@@ -0,0 +1,21 @@
+import os
+from multiprocessing import Pool
+import time
+command1 = "python3 /home/tingyind/cs171/Checkers_ShallowBlue/Tools/AI_Runner.py 7 7 2 l /home/tingyind/cs171/Checkers_ShallowBlue/src/checkers-python/main.py /home/tingyind/cs171/Checkers_ShallowBlue/Tools/Sample_AIs/Poor_AI_368/main.py | tail -n 1" 
+command2 = "python3 /home/tingyind/cs171/Checkers_ShallowBlue/Tools/AI_Runner.py 9 8 2 l /home/tingyind/cs171/Checkers_ShallowBlue/src/checkers-python/main.py /home/tingyind/cs171/Checkers_ShallowBlue/Tools/Sample_AIs/Poor_AI_368/main.py | tail -n 1" 
+command3 = "python3 /home/tingyind/cs171/Checkers_ShallowBlue/Tools/AI_Runner.py 9 8 2 l /home/tingyind/cs171/Checkers_ShallowBlue/src/checkers-python/main.py /home/tingyind/cs171/Checkers_ShallowBlue/Tools/Sample_AIs/Average_AI_368/main.py | tail -n 1" 
+test_time = 10
+
+def run(i):
+	start = time.time()
+	os.system(command1)
+	print(i,time.time()-start)
+
+if __name__ == '__main__':
+	p = Pool(test_time)
+	for i in range(test_time+1):
+		p.apply_async(run, args=(i,))
+	print("Start Running")
+	p.close()
+	p.join()
+	print("Done")

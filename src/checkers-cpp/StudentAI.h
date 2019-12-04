24a8c38195c6833189ffb8c50f0400f2e98a557f
yutin <493806061@qq.com>
2019-12-03 22:02:19 -0800

    header file done

--- src/checkers-cpp/StudentAI.h
+++ src/checkers-cpp/StudentAI.h
@@ -17,6 +17,13 @@ public:
 	extern float theta[28];
 	virtual Move GetMove(Move board);
 	Move minimax_move(vector<Move> moves);
+	Move monte_carlo_tree(vector<Move> moves, int simulate_times, int s_parent);
+	int simulate(Move move, int simulate_times);
+	void undo(Board board, int times);
+	float min_value(Move move, int depth, float alpha, float beta);
+	float max_value(Move move, int depth, float alpha, float beta);
+	float utility(Board board);
+	float[] features(Board board, int color);
 };
 
 #endif //STUDENTAI_H

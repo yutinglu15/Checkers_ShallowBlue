25d23053972ef10bd553449e00d22a55dc6bfc0a
yutin <493806061@qq.com>
2019-12-07 13:08:14 -0800

    select thetas according to board and move order

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -27,7 +27,8 @@ class StudentAI():
 
         self.file = f"{self.col}-{self.row}-{self.color}-{randint(0,500)}-test.txt"
         self.start = time.time()
-        self.theta = self.get_theta()
+        self.theta1, self.theta2 = self.get_theta()
+        self.cutoff = self.get_cutoff()
 
 
         #self.theta = [8.61043154e+00,  4.48291855e+00,  7.78473553e+00, -7.07767178e-14,2.06230092e+00,  1.18768964e+00]#, 0]
@@ -65,19 +66,39 @@ class StudentAI():
         #     f.write(f"Movecount:{self.movecount} Total time:{time.time()-self.start} This move takes:{time.time()-self.time} Depth:{self.depth}\n")
         return move
 
+    def get_cutoff(self):
+        if self.row == 7 and self.col == 7:
+            return (5,3)
+        else:
+            return (6,3)
+
     def get_depth(self):
-        if self.rol == 7 and self.col == 7:
+        if self.row == 7 and self.col == 7:
             return 6
         else:
             return 6
 
     def get_theta(self):
-        if self.rol == 7 and self.col == 7:
-            return [-1.49, 0.41, 0.0, -0.19, -0.07, 0.25, 0.13, 0.0, 0.0, 0.09, -0.28, -0.53, 3.83, -3.95,
-            1.88, 0.93, 0.08, 0.25, 0.17, 0.0, -0.22, 0.0, -0.24, -0.2, -0.02, 0.03, 0.0, 0.0]
+        theta771_start = [-3.05, -1.9, 0.04, -0.85, -0.06, 0.43, 0.21, 0.0, -0.18, 0.56, -0.32, -0.25, 2.9, -2.56, 1.23, 0.06, 0.44, 0.89, 0.67, 0.27, -0.0, 0.0, 0.64, 0.56, 0.07, 0.91, 0.0, 0.0]
+        theta771_mid = [-2.84, -1.09, -0.05, -0.67, 0.22, 0.42, 0.24, 0.0, -0.18, 0.72, 0.26, 0.38, 3.44, -2.88, 1.83, 0.04, 0.3, 0.79, 0.11, -0.02, -0.14, 0.0, 0.36, 0.3, -0.45, 0.9, 0.0, 0.0]
+        theta771_last = [-3.01, -1.01, -0.06, 0.13, 0.42, 0.18, 0.03, 0.0, -0.51, 0.75, 1.31, 1.26, 2.86, -2.55, 1.5, 0.22, 0.05, 0.42, 0.2, -0.06, -0.16, 0.0, 0.53, 0.45, -0.26, 0.44, 0.0, 0.0]
+
+        theta772_start = [2.07, -0.44, 0.29, 0.45, 0.32, -0.46, -0.04, 0.0, 0.37, -0.25, 0.16, 0.17, 0.0, 0.0, -2.18, -0.62, -0.16, -1.0, 0.24, 0.72, 0.2, 0.0, 0.12, 0.04, 0.75, -1.29, 3.36, -2.93]
+        theta772_mid = [1.93, 0.09, 0.16, 0.37, 0.28, -0.18, -0.17, 0.0, 0.02, -0.28, 0.02, 0.13, 0.0, 0.0, -2.3, -1.12, -0.07, -0.63, 0.38, 0.08, 0.16, 0.0, -0.4, 0.43, 0.54, -0.74, 3.47, -3.05]
+        theta772_last = [1.51, 0.44, -0.01, 0.11, 0.16, -0.16, -0.14, 0.0, -0.17, 0.47, -0.16, 0.02, 0.0, 0.0, -2.79, -0.98, 0.0, 0.14, 0.4, -0.08, 0.0, 0.0, -0.26, 1.25, 0.06, -0.07, 2.32, -2.19]
+
+        theta981_start = [-2.85, -0.22, 0.11, -0.54, 0.31, -0.03, 0.07, 0.0, -0.19, 0.45, -0.17, 0.45, 2.09, -1.88, 2.46, 1.24, -0.04, 0.64, 0.41, -0.36, 0.11, 0.25, -0.01, 0.09, -0.34, 0.96, 0.0, 0.0]
+        theta981_mid = [-1.95, -1.35, 0.11, -0.44, 0.11, 0.16, 0.02, 0.0, -0.18, 0.57, 0.26, 0.54, 2.37, -2.06, 1.22, -0.19, 0.3, 0.9, 0.43, 0.09, 0.05, -0.06, 0.19, 0.19, -0.04, 0.69, 0.0, 0.0]
+        theta981_last = [-3.22, -1.85, 0.18, 0.48, 0.23, 0.14, -0.18, 0.0, -0.53, 1.25, 0.93, 0.03, 3.33, -3.01, 2.08, 0.05, 0.15, 0.65, 0.27, 0.03, -0.13, -1.0, -0.24, -0.25, 0.01, 0.03, 0.0, 0.0]
+
+        theta982_start = [1.47, -0.0, 0.21, 0.5, 0.09, -0.09, 0.05, 0.0, 0.12, -0.45, 0.15, 0.55, 0.0, 0.0, -2.5, -0.49, 0.05, -0.45, 0.24, 0.24, 0.29, 0.29, 0.02, 0.12, 0.26, -0.26, 2.31, -2.11]
+        theta982_mid = [1.22, -0.16, 0.29, 0.22, 0.28, -0.21, -0.05, 0.0, 0.38, -0.24, -0.31, 0.59, 0.0, 0.0, -2.04, -1.22, 0.14, -0.54, 0.12, -0.07, 0.05, -0.04, 0.31, 0.57, 0.41, -0.88, 3.14, -2.84]
+        theta982_last = [2.12, -0.19, 0.19, -0.23, 0.33, -0.17, -0.11, 0.0, -0.44, 0.32, 0.16, 0.0, 0.0, 0.0, -2.06, -1.58, 0.09, -0.16, 0.46, -0.19, 0.0, -0.94, -0.7, 0.74, 1.33, -0.08, 4.14, -3.98]
+
+        if self.row == 7 and self.col == 7:
+                return (theta771_start, theta771_mid, theta771_last), (theta772_start, theta772_mid, theta772_last)
         else:
-            return [-1.76, -0.4, 0.03, -0.08, 0.16, 0.3, 0.16, 0.55, -0.38, -0.17, -0.12, 0.28, 2.8, -2.77,
-             1.82, 0.82, 0.22, 0.1, 0.09, -0.38, -0.09, -1.31, 0.78, 0.42, -0.02, 0.15, 0.0, 0.0]
+            return (theta981_start, theta981_mid, theta981_last), (theta982_start, theta982_mid, theta982_last)
 
 
     def minimax_move(self, moves: [list]) :
@@ -85,7 +106,7 @@ class StudentAI():
         max_value = - math.inf
         for chess in moves:
             for move in chess:
-                val = self.max_value(move, self.depth, -math.inf, math.inf)
+                val = self.min_value(move, self.depth, -math.inf, math.inf)
                 if val > max_value:
                     best = [move]
                     max_value = val
@@ -148,11 +169,7 @@ class StudentAI():
 
     def reorder(self, u_list, moves):
         for i in range(len(moves)):
-            #print(u_list)
-            #print(moves[i])
             moves[i] = sorted(moves[i], key=lambda x: u_list[i][moves[i].index(x)])
-            #moves[i] = [m for _,m in sorted(zip(u_list[i], moves[i]))]
-        #return [chess for _,chess in sorted(zip([sum(i)/len(i) for i in u_list], moves))]
         u_list_chess = [sum(i)/len(i) for i in u_list]
         return sorted(moves, key=lambda x: u_list_chess[moves.index(x)])
 
@@ -229,22 +246,36 @@ class StudentAI():
         if self.color == 1:
             wmoveable, weatable = self.moveables(board, 2)
             bmoveable, beatable = 0, 0
-            return sum(x*t for x,t in zip([wcount, wking, wdis, wback, wedge,
+            features = [wcount, wking, wdis, wback, wedge,
             wcenter, wdiag, wdog, wbridge, wuptriangle,
             wdowntriangle, woreo, wmoveable, weatable,
             bcount, bking, bdis, bback, bedge,
              bcenter, bdiag, bdog, bbridge, buptriangle,
-             bdowntriangle, boreo, bmoveable, beatable], self.theta))
+             bdowntriangle, boreo, bmoveable, beatable]
+            if bcount > self.cutoff[0]:
+                return sum(x*t for x,t in zip(features, self.theta1[0]))
+            elif self.cutoff[1] < bcount <= self.cutoff[0]:
+                return sum(x * t for x, t in zip(features, self.theta1[1]))
+            else:
+                return sum(x * t for x, t in zip(features, self.theta1[2]))
+
 
         else:
             wmoveable, weatable = 0, 0
             bmoveable, beatable = self.moveables(board, 1)
-            return sum(x*t for x, t in zip([bcount, bking, bdis, bback, bedge,
-             bcenter, bdiag, bdog, bbridge, buptriangle,
-             bdowntriangle, boreo, bmoveable, beatable,
-            wcount, wking, wdis, wback, wedge,
-            wcenter, wdiag, wdog, wbridge, wuptriangle,
-            wdowntriangle, woreo, wmoveable, weatable], self.theta))
+            features = [wcount, wking, wdis, wback, wedge,
+                       wcenter, wdiag, wdog, wbridge, wuptriangle,
+                       wdowntriangle, woreo, wmoveable, weatable,
+                       bcount, bking, bdis, bback, bedge,
+                       bcenter, bdiag, bdog, bbridge, buptriangle,
+                       bdowntriangle, boreo, bmoveable, beatable]
+            if wcount > self.cutoff[0]:
+                return sum(x*t for x, t in zip(features, self.theta2[0]))
+            elif self.cutoff[1] < wcount <= self.cutoff[0]:
+                return sum(x * t for x, t in zip(features, self.theta2[1]))
+            else:
+                return sum(x * t for x, t in zip(features, self.theta2[2]))
+
 
 
 

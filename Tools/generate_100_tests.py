4c5cf5042544d578d3bb9f0eb6c082322ce9e1d8
Tinameow <dingtingyin@foxmail.com>
2019-12-07 18:07:18 -0800

    add test cpp

--- Tools/generate_100_tests.py
+++ Tools/generate_100_tests.py
@@ -6,6 +6,7 @@ runner = "python3 /home/yutil15/Checkers_ShallowBlue/Tools/AI_Runner.py "
 size1 = "7 7 2 l "
 size2 = "9 8 2 l "
 our_AI = "/home/yutil15/Checkers_ShallowBlue/src/checkers-python/main.py "
+our_cpp = "/home/yutil15/Checkers_ShallowBlue/src/checkers-cpp/main "
 Average_AI = "/home/yutil15/Checkers_ShallowBlue/Tools/Sample_AIs/Average_AI_368/main.py "
 Random_AI = "/home/yutil15/Checkers_ShallowBlue/Tools/Sample_AIs/Random_AI/main.py "
 Good_AI = "/home/yutil15/Checkers_ShallowBlue/Tools/Sample_AIs/Good_AI_368/main.py "
@@ -28,10 +29,17 @@ good2 = runner + size1 + Good_AI + our_AI
 good3 = runner + size2 + our_AI + Good_AI
 good4 = runner + size2 + Good_AI + our_AI
 
+test1cpp = runner + size1 + our_cpp + Average_AI
+test2cpp = runner + size1 + Average_AI + our_cpp
+test3cpp = runner + size2 + our_cpp + Average_AI
+test4cpp = runner + size2 + Average_AI + our_cpp
+
 test_commands = [test1, test2, test3, test4]
 #test_commands = [good1, good2, good3, good4]
 
-test_time = 100
+test_cpps = [test1cpp, test2cpp, test3cpp, test4cpp]
+
+test_time = 10
 
 def rundc(j, dc):
 	# for dc in dc_commands:
@@ -40,8 +48,8 @@ def rundc(j, dc):
 	t = time.time() - start
 	print(i,time.time()-start)
 
-def runtest(j):
-	for i, t in enumerate(test_commands, 1):
+def runtest(j, tests):
+	for i, t in enumerate(tests, 1):
 		start = time.time()
 		os.system(t+tail+' >> result'+str(i)+'.txt')
 		print(i, time.time()-start)
@@ -60,7 +68,7 @@ if __name__ == '__main__':
 
 	p = Pool(test_time)
 	for i in range(test_time+1):
-		p.apply_async(runtest, args=(i,))
+		p.apply_async(runtest, args=(i,test_cpps))
 	print("Start Running")
 	p.close()
 	p.join()

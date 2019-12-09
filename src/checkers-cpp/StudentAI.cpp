6677ee6918f4bc552f66c78551b0e8c28066cfc6
yutin <493806061@qq.com>
2019-12-08 22:22:21 -0800

    add theta for 7x7 in different stage

--- src/checkers-cpp/StudentAI.cpp
+++ src/checkers-cpp/StudentAI.cpp
@@ -224,8 +224,81 @@ namespace
         for (int i = 0; i < 14; i++)
             result += wcount[i]*theta[i];
         for (int i = 14; i < 28; i++)
-            result += bcount[i]*theta[i];
+            result += bcount[i-14]*theta[i];
         return result;
+    }
+
+    double** get_theta_first(int col, int row)
+    {
+        double theta771_start[] = {-3.05, -1.9, 0.04, -0.85, -0.06, 0.43, 0.21, 0.0, -0.18, 0.56, -0.32, -0.25, 2.9, -2.56, 1.23,
+                          0.06, 0.44, 0.89, 0.67, 0.27, -0.0, 0.0, 0.64, 0.56, 0.07, 0.91, 0.0, 0.0};
+        double theta771_mid[] = {-2.84, -1.09, -0.05, -0.67, 0.22, 0.42, 0.24, 0.0, -0.18, 0.72, 0.26, 0.38, 3.44, -2.88, 1.83,
+                        0.04, 0.3, 0.79, 0.11, -0.02, -0.14, 0.0, 0.36, 0.3, -0.45, 0.9, 0.0, 0.0};
+        double theta771_last[] = {-3.01, -1.01, -0.06, 0.13, 0.42, 0.18, 0.03, 0.0, -0.51, 0.75, 1.31, 1.26, 2.86, -2.55, 1.5,
+                         0.22, 0.05, 0.42, 0.2, -0.06, -0.16, 0.0, 0.53, 0.45, -0.26, 0.44, 0.0, 0.0};
+
+
+
+        double theta981_start[] = {-2.85, -0.22, 0.11, -0.54, 0.31, -0.03, 0.07, 0.0, -0.19, 0.45, -0.17, 0.45, 2.09, -1.88,
+                          2.46, 1.24, -0.04, 0.64, 0.41, -0.36, 0.11, 0.25, -0.01, 0.09, -0.34, 0.96, 0.0, 0.0};
+        double theta981_mid[] = {-1.95, -1.35, 0.11, -0.44, 0.11, 0.16, 0.02, 0.0, -0.18, 0.57, 0.26, 0.54, 2.37, -2.06, 1.22,
+                        -0.19, 0.3, 0.9, 0.43, 0.09, 0.05, -0.06, 0.19, 0.19, -0.04, 0.69, 0.0, 0.0};
+        double theta981_last[] = {-3.22, -1.85, 0.18, 0.48, 0.23, 0.14, -0.18, 0.0, -0.53, 1.25, 0.93, 0.03, 3.33, -3.01, 2.08,
+                         0.05, 0.15, 0.65, 0.27, 0.03, -0.13, -1.0, -0.24, -0.25, 0.01, 0.03, 0.0, 0.0};
+
+
+
+        double** start_mid_last;
+
+        if (col == 7 && row == 7)
+        {
+            start_mid_last[0] = theta771_start;
+            start_mid_last[1] = theta771_mid;
+            start_mid_last[2] = theta771_last;
+            return start_mid_last;
+        }
+        else
+        {
+            start_mid_last[0] = theta981_start;
+            start_mid_last[1] = theta981_mid;
+            start_mid_last[2] = theta981_last;
+            return start_mid_last;
+        }
+
+    }
+
+    double** get_theta_backhand(int row, int col)
+    {
+        double theta772_start[] = {2.07, -0.44, 0.29, 0.45, 0.32, -0.46, -0.04, 0.0, 0.37, -0.25, 0.16, 0.17, 0.0, 0.0, -2.18,
+                          -0.62, -0.16, -1.0, 0.24, 0.72, 0.2, 0.0, 0.12, 0.04, 0.75, -1.29, 3.36, -2.93};
+        double theta772_mid[] = {1.93, 0.09, 0.16, 0.37, 0.28, -0.18, -0.17, 0.0, 0.02, -0.28, 0.02, 0.13, 0.0, 0.0, -2.3, -1.12,
+                        -0.07, -0.63, 0.38, 0.08, 0.16, 0.0, -0.4, 0.43, 0.54, -0.74, 3.47, -3.05};
+        double theta772_last[] = {1.51, 0.44, -0.01, 0.11, 0.16, -0.16, -0.14, 0.0, -0.17, 0.47, -0.16, 0.02, 0.0, 0.0, -2.79,
+                         -0.98, 0.0, 0.14, 0.4, -0.08, 0.0, 0.0, -0.26, 1.25, 0.06, -0.07, 2.32, -2.19};
+
+        double theta982_start[] = {1.47, -0.0, 0.21, 0.5, 0.09, -0.09, 0.05, 0.0, 0.12, -0.45, 0.15, 0.55, 0.0, 0.0, -2.5, -0.49,
+                          0.05, -0.45, 0.24, 0.24, 0.29, 0.29, 0.02, 0.12, 0.26, -0.26, 2.31, -2.11};
+        double theta982_mid[] = {1.22, -0.16, 0.29, 0.22, 0.28, -0.21, -0.05, 0.0, 0.38, -0.24, -0.31, 0.59, 0.0, 0.0, -2.04,
+                        -1.22, 0.14, -0.54, 0.12, -0.07, 0.05, -0.04, 0.31, 0.57, 0.41, -0.88, 3.14, -2.84};
+        double theta982_last[] = {2.12, -0.19, 0.19, -0.23, 0.33, -0.17, -0.11, 0.0, -0.44, 0.32, 0.16, 0.0, 0.0, 0.0, -2.06,
+                         -1.58, 0.09, -0.16, 0.46, -0.19, 0.0, -0.94, -0.7, 0.74, 1.33, -0.08, 4.14, -3.98};
+
+        double** start_mid_last;
+
+        if (col == 7 && row == 7)
+        {
+            start_mid_last[0] = theta772_start;
+            start_mid_last[1] = theta772_mid;
+            start_mid_last[2] = theta772_last;
+            return start_mid_last;
+        }
+        else
+        {
+            start_mid_last[0] = theta982_start;
+            start_mid_last[1] = theta982_mid;
+            start_mid_last[2] = theta982_last;
+            return start_mid_last;
+        }
     }
 
 
@@ -243,7 +316,7 @@ namespace
 
 //The following part should be completed by students.
 //The students can modify anything except the class name and exisiting functions and varibles.
-StudentAI::StudentAI(int col,int row,int p)
+StudentAI::StudentAI(int col,int row, int p)
 	:AI(col, row, p)
 {
     board = Board(col,row,p);
@@ -252,11 +325,22 @@ StudentAI::StudentAI(int col,int row,int p)
     if (col == 7) depth = 6;
     else
         depth = 4;
+//    cutoff[] = {5,3};
+
     // I don't remember!!!!! Lucy NB!
-    theta = new double[28]{-24.13, -7.87, -17.89, -16.67, -6.99, 7.22, 1.19, 0.72,
-                       -4.2, -4.52, -2.49, -3.14, 5.69, 0.02, 3.53, -3.58, 9.37,
-                       -3.81, -1.58, -1.75, 2.51, 0.26, 18.3, 10.25, 3.63,
-                       3.69, 1.32, -4.03};
+    theta771_start = new double[28]{-3.05, -1.9, 0.04, -0.85, -0.06, 0.43, 0.21, 0.0, -0.18, 0.56, -0.32, -0.25, 2.9, -2.56, 1.23,
+                      0.06, 0.44, 0.89, 0.67, 0.27, -0.0, 0.0, 0.64, 0.56, 0.07, 0.91, 0.0, 0.0};
+    theta771_mid = new double[28]{-2.84, -1.09, -0.05, -0.67, 0.22, 0.42, 0.24, 0.0, -0.18, 0.72, 0.26, 0.38, 3.44, -2.88, 1.83,
+                    0.04, 0.3, 0.79, 0.11, -0.02, -0.14, 0.0, 0.36, 0.3, -0.45, 0.9, 0.0, 0.0};
+    theta771_last = new double[28]{-3.01, -1.01, -0.06, 0.13, 0.42, 0.18, 0.03, 0.0, -0.51, 0.75, 1.31, 1.26, 2.86, -2.55, 1.5,
+                     0.22, 0.05, 0.42, 0.2, -0.06, -0.16, 0.0, 0.53, 0.45, -0.26, 0.44, 0.0, 0.0};
+
+    theta772_start = new double[28]{2.07, -0.44, 0.29, 0.45, 0.32, -0.46, -0.04, 0.0, 0.37, -0.25, 0.16, 0.17, 0.0, 0.0, -2.18,
+                      -0.62, -0.16, -1.0, 0.24, 0.72, 0.2, 0.0, 0.12, 0.04, 0.75, -1.29, 3.36, -2.93};
+    theta772_mid = new double[28]{1.93, 0.09, 0.16, 0.37, 0.28, -0.18, -0.17, 0.0, 0.02, -0.28, 0.02, 0.13, 0.0, 0.0, -2.3, -1.12,
+                    -0.07, -0.63, 0.38, 0.08, 0.16, 0.0, -0.4, 0.43, 0.54, -0.74, 3.47, -3.05};
+    theta772_last = new double[28]{1.51, 0.44, -0.01, 0.11, 0.16, -0.16, -0.14, 0.0, -0.17, 0.47, -0.16, 0.02, 0.0, 0.0, -2.79,
+                     -0.98, 0.0, 0.14, 0.4, -0.08, 0.0, 0.0, -0.26, 1.25, 0.06, -0.07, 2.32, -2.19};
 }
 
 Move StudentAI::GetMove(Move move)
@@ -303,7 +387,10 @@ Move StudentAI::minimaxMove(const vector<vector<Move> > & moves)
 double StudentAI::get_min(const Move& move, int depth, double alpha, double beta)
 {
     if (depth == 0)
-        return utility(board, player);
+        if (row == 7)
+            return utility(board, player);
+        else
+            return basic_utility(board, player);
 
     vector<vector<Move>> moves = board.getAllPossibleMoves(player == 1?2:1);
 
@@ -328,7 +415,10 @@ double StudentAI::get_min(const Move& move, int depth, double alpha, double beta
 double StudentAI::get_max(const Move& move, int depth, double alpha, double beta)
 {
     if (depth == 0)
-        return utility(board, player == 1?2:1);
+        if (row == 7)
+            return utility(board, player);
+        else
+            return basic_utility(board, player);
 
     vector<vector<Move>> moves = board.getAllPossibleMoves(player);
 
@@ -363,9 +453,10 @@ double StudentAI::utility(Board & board, int player) const
 //    [wcount, wking, wdis, wback, wedge,
 //            wcenter, wdiag, wdog, wbridge, wuptriangle,
 //            wdowntriangle, woreo, wmoveable, weatable]
-    int wcount[14];
+    int wcount[14] = {0};
     wcount[0] = board.whiteCount;
-    int bcount[14];
+    int bcount[14] = {0};
+//    double[][] theta;
     bcount[0] = board.blackCount;
     wking_bking(board, wcount, bcount);
     wdis_bdis(board, wcount, bcount);
@@ -384,14 +475,28 @@ double StudentAI::utility(Board & board, int player) const
         moveables(board, 2, wcount);
         bcount[12] = 0;
         bcount[13] = 0;
+//        double** theta_start_mid_last = get_theta_first(row, col);
+        if (bcount[0] > 5)
+            return calculate_utility(wcount, bcount, theta771_start);
+        else if (bcount[0] > 3 && bcount[0] <= 5)
+            return calculate_utility(wcount, bcount, theta771_mid);
+        else
+            return calculate_utility(wcount, bcount, theta771_last);
     }
     else
     {
         wcount[12] = 0;
         wcount[13] = 0;
         moveables(board, 1, bcount);
+//        double** theta_start_mid_last = get_theta_backhand(row, col);
+        if (wcount[0] > 5)
+            return calculate_utility(wcount, bcount, theta772_start);
+        else if (wcount[0] > 3 && wcount[0] <= 5)
+            return calculate_utility(wcount, bcount, theta772_mid);
+        else
+            return calculate_utility(wcount, bcount, theta772_last);
     }
 
-    return calculate_utility(wcount, bcount, theta);
+//    return calculate_utility(wcount, bcount, theta);
 
 }
